dc25e94eae5f4985d321f4e0c3a7a99711843792
Derived Cat <hooyuser@outlook.com>
2022-01-06 16:48:25 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,53 +16,26 @@ const float dt = 0.01;
 const float G_dt = G * dt;
 
 const __m512 eps2 = _mm512_set1_ps(eps * eps);
-const __m512 dt_vec = _mm512_set1_ps(dt);
+const __m512 vec_dt = _mm512_set1_ps(dt);
 const __m512 vec_G_dt = _mm512_set1_ps(G_dt);
 
 
 struct Star_16 {
-	alignas(64) __m512 px;
-	alignas(64) __m512 py;
-	alignas(64) __m512 pz;
-	alignas(64) __m512 mass;
 	alignas(64) __m512 vx;
 	alignas(64) __m512 vy;
 	alignas(64) __m512 vz;
+
+	alignas(64) __m512 px;
+	alignas(64) __m512 py;
+	alignas(64) __m512 pz;
+	
+	alignas(64) __m512 mass;
 };
 
 std::array<Star_16, STAR_16_NUM> stars;
 
 static float frand() {
 	return (float)rand() / RAND_MAX * 2 - 1;
-}
-
-static float reduce512_add_ps(__m512 zmm) {
-	__m256 high = _mm512_extractf32x8_ps(zmm, 1);
-	__m256 low = _mm512_castps512_ps256(zmm);
-	__m256 sumOct = _mm256_add_ps(low, high);
-
-	__m128 vlow = _mm256_castps256_ps128(sumOct);
-	// vlow = ( x3, x2, x1, x0 )
-
-	__m128 vhigh = _mm256_extractf128_ps(sumOct, 1);
-	// vhigh = ( x7, x6, x5, x4 )
-
-	vlow = _mm_add_ps(vlow, vhigh);
-	// vlow = ( x3 + x7, x2 + x6, x1 + x5, x0 + x4 )
-
-	__m128 shuf = _mm_movehdup_ps(vlow);
-	// shuf = ( x3 + x7, x3 + x7, x1 + x5, x1 + x5 )
-
-	__m128 sums = _mm_add_ps(vlow, shuf);
-	// sum = ( - , x2 + x6 + x3 + x7, - , x0 + x4 + x1 + x5)
-
-	shuf = _mm_movehl_ps(shuf, sums); // result = (shuf high 64 bits, sums high 64 bits)
-	// shuf = ( x3 + x7, x3 + x7, - , x2 + x6 + x3 + x7 )
-
-	sums = _mm_add_ss(sums, shuf);
-	// sum = ( -, -, -, x0 + x1 + x2 + x3 + x4 + x5 + x6 + x7 )
-
-	return _mm_cvtss_f32(sums);
 }
 
 void init() {
@@ -99,7 +72,9 @@ constexpr static void UNROLL(Fn fn) {
 }
 
 void step() {
-	
+	std::array<__m512, STAR_16_NUM> d_vx{};
+	std::array<__m512, STAR_16_NUM> d_vy{};
+	std::array<__m512, STAR_16_NUM> d_vz{};
 	UNROLL<stars.size(), 1>([&](size_t j) {
 		auto& star_j = stars[j];
 		UNROLL<SIMD_WIDTH, 1>([&](size_t k) {		
@@ -107,10 +82,6 @@ void step() {
 			__m512 py_jk = _mm512_set1_ps(star_j.py.m512_f32[k]);
 			__m512 pz_jk = _mm512_set1_ps(star_j.pz.m512_f32[k]);
 			__m512 mass_jk = _mm512_set1_ps(star_j.mass.m512_f32[k]);
-
-			//__m512 d_vx = _mm512_setzero_ps();  //d_vx = 0
-			//__m512 d_vy = _mm512_setzero_ps();	//d_vy = 0
-			//__m512 d_vz = _mm512_setzero_ps();	//d_vz = 0
 
 			UNROLL<stars.size()>([&](size_t i) {  //unrolling size: 3/1
 				__m512 dx = _mm512_sub_ps(px_jk, stars[i].px);
@@ -132,38 +103,27 @@ void step() {
 				//float inverse_sqrt = 1 / sqrt(prod)^3;
 
 				__m512 factor = _mm512_mul_ps(inverse_sqrt3, mass_jk);
-				factor = _mm512_mul_ps(vec_G_dt, factor);
+				//factor = _mm512_mul_ps(vec_G_dt, factor);
 				//float factor = mass[j] / sqrt(prod)^3;
 
-				/*d_vx = _mm512_fmadd_ps(dx, factor, d_vx);
-				d_vy = _mm512_fmadd_ps(dy, factor, d_vy);
-				d_vz = _mm512_fmadd_ps(dz, factor, d_vz);*/
-
-				stars[i].vx = _mm512_fmadd_ps(dx, factor, stars[i].vx);
-				stars[i].vy = _mm512_fmadd_ps(dy, factor, stars[i].vy);
-				stars[i].vz = _mm512_fmadd_ps(dz, factor, stars[i].vz);
-
-		/*		_mm512_store_ps(&vx[i], new_vx_i);
-				_mm512_store_ps(&vy[i], new_vy_i);
-				_mm512_store_ps(&vz[i], new_vz_i);*/
+				d_vx[i] = _mm512_fmadd_ps(dx, factor, d_vx[i]);
+				d_vy[i] = _mm512_fmadd_ps(dy, factor, d_vy[i]);
+				d_vz[i] = _mm512_fmadd_ps(dz, factor, d_vz[i]);
 				//d_vx += dx * factor;
 				//d_vy += dy * factor; 
 				//d_vz += dz * factor; 
 				});
-
-			//star_i.vx.m512_f32[k] += reduce512_add_ps(d_vx) * G_dt;
-			//star_i.vy.m512_f32[k] += reduce512_add_ps(d_vy) * G_dt;
-			//star_i.vz.m512_f32[k] += reduce512_add_ps(d_vz) * G_dt;
-			//star_i.vx.m512_f32[k] += _mm512_reduce_add_ps(d_vx) * G_dt;  //alternatives
-			//star_i.vy.m512_f32[k] += _mm512_reduce_add_ps(d_vy) * G_dt;
-			//star_i.vz.m512_f32[k] += _mm512_reduce_add_ps(d_vz) * G_dt;
 			});
 		});
 
 	UNROLL<stars.size()>([&](size_t i) {   //unrolling size: 3/1
-		stars[i].px = _mm512_fmadd_ps(stars[i].vx, dt_vec, stars[i].px);
-		stars[i].py = _mm512_fmadd_ps(stars[i].vy, dt_vec, stars[i].py);
-		stars[i].pz = _mm512_fmadd_ps(stars[i].vz, dt_vec, stars[i].pz);
+		stars[i].vx = _mm512_fmadd_ps(d_vx[i], vec_G_dt, stars[i].vx);
+		stars[i].vy = _mm512_fmadd_ps(d_vy[i], vec_G_dt, stars[i].vy);
+		stars[i].vz = _mm512_fmadd_ps(d_vz[i], vec_G_dt, stars[i].vz);
+
+		stars[i].px = _mm512_fmadd_ps(stars[i].vx, vec_dt, stars[i].px);
+		stars[i].py = _mm512_fmadd_ps(stars[i].vy, vec_dt, stars[i].py);
+		stars[i].pz = _mm512_fmadd_ps(stars[i].vz, vec_dt, stars[i].pz);
 		//px[i] += vx[i] * dt;
 		//py[i] += vy[i] * dt;
 		//pz[i] += vz[i] * dt;
@@ -217,7 +177,7 @@ int main() {
 	init();
 	printf("Initial energy: %f\n", calc());  // Initial energy: -8.571527
 	auto const dt = benchmark([&] {
-		for (size_t i = 0; i < 1000000; i++)
+		for (size_t i = 0; i < 2000000; i++)
 			step();
 		});
 	printf("Final energy: %f\n", calc());  // Final energy: -8.562095
